c417ed7c3048cc00b843dee088f039434eef1828
raghavendramv1009-dev <raghavendramv1009@gmail.com>
2026-02-19 21:37:35 +0530

    change in index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 //const bodyParser = require('body-parser');
-const connectDB = require('./configs/mongooseDB');
+const connectDB = require('./Configs/MongooseDB');
 
 const usersRoutes = require('./Routes/UsersRoutes');
 // const categoryRoutes = require('./routes/categoryRoutes');
